6a870454dda13024534b16422738863e57faa344
Gerardo Hernandez <gerardohcampos@gmail.com>
2016-07-31 00:07:09 +0100

    Parsing of Visual Studio files is now completed

--- extractheaders/vsparsing.cpp
+++ extractheaders/vsparsing.cpp
@@ -1,30 +1,72 @@
 #include "vsparsing.h"
-#include "tinyxml2.h"
+#include <stdexcept>
+#include <vector>
+#include <string>
+#include <regex>
 
 using namespace tinyxml2;
+using namespace std;
 
 VsParsing::VsParsing(const char* path)
-{
-	XMLDocument doc;
-	XMLElement* project;
-	XMLElement* itemGroup;
+{				
+	if (doc.LoadFile(path) != XMLError::XML_SUCCESS)
+		throw runtime_error(string("Cannot open: ") + path + ": " + doc.ErrorName());
 	
 
-	doc.LoadFile(path);
-	project = doc.FirstChildElement("Project");
-	itemGroup = project->FirstChildElement("ItemGroup");
+}
 
-	while (true) {
+void VsParsing::parse(vector<ProjectConfiguration>& configurations,
+	                  vector<string>& files)
+{
+	XMLElement* project = project = doc.FirstChildElement("Project");;
+	XMLElement* itemGroup = project->FirstChildElement("ItemGroup");
+	XMLElement* itemDefinitionGroup = project->FirstChildElement("ItemDefinitionGroup");
+
+	while (itemGroup) {
+		const char* labelItemGroup;
 		XMLElement* clCompile;
-		
-		if (clCompile = itemGroup->FirstChildElement("ClCompile")) {
-			const char* source = clCompile->Attribute("Include");
-			int i;
 
-			i = i;
+		if ((labelItemGroup = itemGroup->Attribute("Label")) && !strcmp(labelItemGroup, "ProjectConfigurations")) {
+			XMLElement* projectConfiguration = itemGroup->FirstChildElement("ProjectConfiguration");
+
+			while (projectConfiguration) {
+				const char* label = projectConfiguration->Attribute("Include");
+
+				configurations.push_back({label});
+				projectConfiguration = projectConfiguration->NextSiblingElement("ProjectConfiguration");
+			}
 		}
+
+		clCompile = itemGroup->FirstChildElement("ClCompile");
+
+		while (clCompile) {
+			files.push_back(clCompile->Attribute("Include"));
+			clCompile = clCompile->NextSiblingElement("ClCompile");
+		}
+		
 		itemGroup = itemGroup->NextSiblingElement("ItemGroup");
 	}
-		//->FirstChildElement("TITLE")->GetText();
+	
+	while (itemDefinitionGroup) {
+		for (auto& configuration : configurations) {			
+			const char* label = itemDefinitionGroup->Attribute("Condition");
+
+			if (label == "'$(Configuration)|$(Platform)'=='" + configuration.name + "'") {
+				XMLElement* clCompile = itemDefinitionGroup->FirstChildElement("ClCompile");
+				XMLElement* definitions = clCompile? clCompile->FirstChildElement("PreprocessorDefinitions") : NULL;
+
+				if (definitions) {
+					configuration.definitions = definitions->FirstChild()->ToText()->Value();
+
+					// replace things like %(PreprocessorDefinitions) which extract headers cannot understand
+					configuration.definitions = regex_replace(configuration.definitions, regex("%\\(.*\\)"), string(""));
+					
+				}
+			}
+			itemDefinitionGroup = itemDefinitionGroup->NextSiblingElement("ItemDefinitionGroup");
+		}
+	}	
+
 
 }
+
