923112e2e069d9a972c1c3866d14f038ad969bcf
Gerardo Hernandez <gerardohcampos@gmail.com>
2016-08-21 11:10:08 +0100

    Added missing vs_parsing.h to the commit

--- extractheaders/vsparsing.h
+++ extractheaders/vsparsing.h
@@ -1,11 +1,27 @@
 #ifndef VSPARSING_H
 #define VSPARSING_H
+#include <string>
+#include <map>
+#include <vector>
+#include "tinyxml2.h"
 
+struct ProjectConfiguration {
+	std::string name;
+	std::string definitions;
+	std::string additionalIncludeDirectories;
+};
 
 class VsParsing {
 public:
 
+	// @throw std::runtime_error If the file could not be opened
 	VsParsing(const char* path);
+	void parse(std::vector<ProjectConfiguration>& configurations,
+		       std::vector<std::string>& files);
+private:
+
+	tinyxml2::XMLDocument doc;
+
 };
 
 #endif
