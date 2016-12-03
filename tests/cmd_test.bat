18f7208e9b11dfdc7792cd4bab33b43ed4d582e7
Gerardo Hernandez <gerardohcampos@gmail.com>
2016-12-03 18:07:07 +0000

    Fixed qt path in cmd_test.bat

--- tests/cmd_test.bat
+++ tests/cmd_test.bat
@@ -1,7 +1,8 @@
 @echo off
 
-rm readmeexample_test\pct_test\stdafx.h
-..\extractheaderscmd\debug\extractheaders --input readmeexample_test\pct_test\file.cpp --def "WIN32;WIN32;_M_X64" --sysinclude "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\include" --sysinclude "C:\Qt\5.6\5.6\msvc2013_64\include" -o readmeexample_test\pct_test\stdafx.h
+diff
+rm -f readmeexample_test\pct_test\stdafx.h
+..\extractheaderscmd\debug\extractheaders --input readmeexample_test\pct_test\file.cpp --def "WIN32;WIN32;_M_X64" --sysinclude "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\include" --sysinclude "C:\Qt\5.6\msvc2015_64\include" -o readmeexample_test\pct_test\stdafx.h
 
 IF %errorlevel% neq 0 (
    echo extractheaders failed with error code: %errorlevel% 
