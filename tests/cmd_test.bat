3d1dd4252ed9a228ae33fa1e861cfbce6818aaa2
Gerardo Hernandez <gerardohcampos@gmail.com>
2016-12-03 18:09:58 +0000

    Now tests use the default diff tool installed in appveyor images, instead of the one that was added to the repository

--- tests/cmd_test.bat
+++ tests/cmd_test.bat
@@ -1,6 +1,5 @@
 @echo off
 
-diff
 rm -f readmeexample_test\pct_test\stdafx.h
 ..\extractheaderscmd\debug\extractheaders --input readmeexample_test\pct_test\file.cpp --def "WIN32;WIN32;_M_X64" --sysinclude "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\include" --sysinclude "C:\Qt\5.6\msvc2015_64\include" -o readmeexample_test\pct_test\stdafx.h
 
@@ -10,7 +9,7 @@ IF %errorlevel% neq 0 (
 )
 
 echo Comparing output with reference...
-diffutils\bin\diff readmeexample_test\pct_test\stdafx.h readmeexample_test\pct_test\expected.h
+diff readmeexample_test\pct_test\stdafx.h readmeexample_test\pct_test\expected.h
 echo.
 
 IF %errorlevel% equ 0 (
