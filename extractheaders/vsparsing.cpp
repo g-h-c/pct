9ae43058bb737f4298dec4a465c3ca5fd6972fc9
g-h-c <ghc.cpp@gmail.com>
2016-12-29 13:11:29 +0100

    - Now when reading .vcproj files (Visual Studio 2008 or previous) a warning is displayed instead of crashing

--- extractheaders/vsparsing.cpp
+++ extractheaders/vsparsing.cpp
@@ -1,4 +1,5 @@
 #include "vsparsing.h"
+#include <boost/filesystem/path.hpp>
 #include <stdexcept>
 #include <vector>
 #include <string>
@@ -7,16 +8,21 @@
 
 
 using namespace tinyxml2;
+using namespace boost::filesystem;
 using namespace std;
 
 VcxprojParsing::VcxprojParsing(const char* path,
 	                           std::ostream& errStream)
 	: errorStream(errStream)
-{				
+{			
+	boost::filesystem::path filepath;
+	
 	if (doc.LoadFile(path) != XMLError::XML_SUCCESS)
 		throw runtime_error(string("Cannot open: ") + path + ": " + doc.ErrorName());
 	
-
+	filepath = boost::filesystem::path(path);	
+	if (filepath.extension().string() == ".vcproj")
+		errStream << "Warning: when reading: " << path << ". vcproj files are not supported. Please upgrade your solution to a Visual Studio 2010 solution or later";
 }
 
 void VcxprojParsing::replaceEnvVars(string& paths)
@@ -73,69 +79,70 @@ void VcxprojParsing::replaceEnvVars(string& paths)
 void VcxprojParsing::parse(vector<ProjectConfiguration>& configurations,
 	                  vector<string>& files)
 {
-	XMLElement* project = project = doc.FirstChildElement("Project");;
-	XMLElement* itemGroup = project->FirstChildElement("ItemGroup");
-	XMLElement* itemDefinitionGroup = project->FirstChildElement("ItemDefinitionGroup");
+	XMLElement* project = project = doc.FirstChildElement("Project");
 
-	while (itemGroup) {
-		const char* labelItemGroup;
-		XMLElement* clCompile;
+	if (project) {
+		XMLElement* itemGroup = project->FirstChildElement("ItemGroup");
+		XMLElement* itemDefinitionGroup = project->FirstChildElement("ItemDefinitionGroup");
 
-		if ((labelItemGroup = itemGroup->Attribute("Label")) && !strcmp(labelItemGroup, "ProjectConfigurations")) {
-			XMLElement* projectConfiguration = itemGroup->FirstChildElement("ProjectConfiguration");
+		while (itemGroup) {
+			const char* labelItemGroup;
+			XMLElement* clCompile;
 
-			while (projectConfiguration) {
-				const char* label = projectConfiguration->Attribute("Include");
-				const char* configurationName = projectConfiguration->FirstChildElement("Configuration")->GetText();
+			if ((labelItemGroup = itemGroup->Attribute("Label")) && !strcmp(labelItemGroup, "ProjectConfigurations")) {
+				XMLElement* projectConfiguration = itemGroup->FirstChildElement("ProjectConfiguration");
 
-				configurations.push_back({label, configurationName});
-				projectConfiguration = projectConfiguration->NextSiblingElement("ProjectConfiguration");
+				while (projectConfiguration) {
+					const char* label = projectConfiguration->Attribute("Include");
+					const char* configurationName = projectConfiguration->FirstChildElement("Configuration")->GetText();
+
+					configurations.push_back({ label, configurationName });
+					projectConfiguration = projectConfiguration->NextSiblingElement("ProjectConfiguration");
+				}
+			}
+
+			clCompile = itemGroup->FirstChildElement("ClCompile");
+
+			while (clCompile) {
+				files.push_back(clCompile->Attribute("Include"));
+				clCompile = clCompile->NextSiblingElement("ClCompile");
+			}
+
+			itemGroup = itemGroup->NextSiblingElement("ItemGroup");
+		}
+
+		while (itemDefinitionGroup) {
+			for (auto& configuration : configurations) {
+				const char* label = itemDefinitionGroup->Attribute("Condition");
+
+				if (label == "'$(Configuration)|$(Platform)'=='" + configuration.name + "'") {
+					XMLElement* clCompile = itemDefinitionGroup->FirstChildElement("ClCompile");
+					XMLElement* definitions = clCompile ? clCompile->FirstChildElement("PreprocessorDefinitions") : NULL;
+					XMLElement* includeDirs = clCompile ? clCompile->FirstChildElement("AdditionalIncludeDirectories") : NULL;
+					XMLElement* precompiledHeaderFile = clCompile ? clCompile->FirstChildElement("PrecompiledHeaderFile") : NULL;
+
+					if (definitions && definitions->FirstChild()) {
+						configuration.definitions = definitions->FirstChild()->ToText()->Value();
+
+						// replace things like %(PreprocessorDefinitions) which extract headers cannot understand
+						configuration.definitions = regex_replace(configuration.definitions, regex("%\\(.*\\)"), string(""));
+					}
+
+					if (includeDirs && includeDirs->FirstChild()) {
+						configuration.additionalIncludeDirectories = includeDirs->FirstChild()->ToText()->Value();
+						replaceEnvVars(configuration.additionalIncludeDirectories);
+					}
+
+					if (precompiledHeaderFile && precompiledHeaderFile->FirstChild()) {
+						configuration.precompiledHeaderFile = precompiledHeaderFile->FirstChild()->ToText()->Value();
+						replaceEnvVars(configuration.precompiledHeaderFile);
+					}
+
+				}
+				itemDefinitionGroup = itemDefinitionGroup->NextSiblingElement("ItemDefinitionGroup");
 			}
 		}
-
-		clCompile = itemGroup->FirstChildElement("ClCompile");
-
-		while (clCompile) {
-			files.push_back(clCompile->Attribute("Include"));
-			clCompile = clCompile->NextSiblingElement("ClCompile");
-		}
-		
-		itemGroup = itemGroup->NextSiblingElement("ItemGroup");
 	}
-	
-	while (itemDefinitionGroup) {
-		for (auto& configuration : configurations) {			
-			const char* label = itemDefinitionGroup->Attribute("Condition");
-
-			if (label == "'$(Configuration)|$(Platform)'=='" + configuration.name + "'") {
-				XMLElement* clCompile = itemDefinitionGroup->FirstChildElement("ClCompile");
-				XMLElement* definitions = clCompile? clCompile->FirstChildElement("PreprocessorDefinitions") : NULL;
-				XMLElement* includeDirs = clCompile ? clCompile->FirstChildElement("AdditionalIncludeDirectories") : NULL;
-				XMLElement* precompiledHeaderFile = clCompile ? clCompile->FirstChildElement("PrecompiledHeaderFile") : NULL;
-
-				if (definitions && definitions->FirstChild()) {
-					configuration.definitions = definitions->FirstChild()->ToText()->Value();
-
-					// replace things like %(PreprocessorDefinitions) which extract headers cannot understand
-					configuration.definitions = regex_replace(configuration.definitions, regex("%\\(.*\\)"), string(""));					
-				}
-
-				if (includeDirs && includeDirs->FirstChild()) {
-					configuration.additionalIncludeDirectories = includeDirs->FirstChild()->ToText()->Value();
-					replaceEnvVars(configuration.additionalIncludeDirectories);					
-				}
-
-				if (precompiledHeaderFile && precompiledHeaderFile->FirstChild()) {
-					configuration.precompiledHeaderFile = precompiledHeaderFile->FirstChild()->ToText()->Value();
-					replaceEnvVars(configuration.precompiledHeaderFile);
-				}
-				
-			}
-			itemDefinitionGroup = itemDefinitionGroup->NextSiblingElement("ItemDefinitionGroup");
-		}
-	}	
-
-
 }
 
 SlnParsing::SlnParsing(const char* path, std::ostream& errStream)
